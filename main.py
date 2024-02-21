e07440abd9763b3fdcee707107997cd8ec618868
barthelemy.ehui <barthelemy.ehui@gmail.com>
2024-02-21 16:19:45 +0100

    update main.py for counting words and letters

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-def main():
-    words = 0
+
+def alphabetLetterCount():
+    tabArr = {}
+    content = getFrankensteinContent().lower()
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] 
+    for char in alphabet:
+        currentCount = 0
+        for charContent in content:
+            if (charContent == char):
+                currentCount += 1
+        tabArr[char] = currentCount
+    return tabArr
+
+    
+    
+
+def getFrankensteinContent():
+    content = ""
     # Open the file using with
     with open('./books/frankenstein.txt', 'r') as file:
         #Read the content of the file 
-        content = file.read()
+        content += file.read()
         # Print the content 
         # print(content)
-        words = len(content.split())
-    return words
+        # words = len(content.split())
+    return content
 
-def numCharacters():
-    
 
-words = main()
-print(words)
+def getWords():
+    content = getFrankensteinContent()
+    return content.split()
+
+
+def printReport():
+    print('--- Begin report of books/frankenstein.txt ---')
+    countWord = len(getWords())
+    print(f'{countWord} words found in the document\n')
+    alphabetLetterCounts = alphabetLetterCount() 
+    for letter, letterCount in alphabetLetterCounts.items():
+        print(f"The {letter} character was found {letterCount} times") 
+    print('--- End report ---')
+
+def main():
+    printReport()
+
+
+main()
